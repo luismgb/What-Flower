1f05c8722f4300be8b796e708ad55ec408d1cf76
luismgb <luismigelgb@gmail.com>
2018-08-28 21:13:29 -0700

    Setup basic UI

--- What Flower/ViewController.swift
+++ What Flower/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pickedImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func takePicture(_ sender: UIBarButtonItem) {
     }
-
-
+    
 }
 
